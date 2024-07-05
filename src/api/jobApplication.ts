50a4c4e3d58b74ea924710c18c271c90162152eb
Ronath Mukadange <ronath.mukadange@gmail.com>
2024-07-05 13:15:44 +0530

    Sample

--- src/api/jobApplication.ts
+++
@@ -1,8 +0,0 @@
-import express from "express";
-import { createJobApplication, getJobApplications } from "../application/jobApplication";
-
-const jobApplicationRouter = express.Router();
-
-jobApplicationRouter.route("/").post(createJobApplication).get(getJobApplications)
-
-export default jobApplicationRouter;
--- src/api/jobs.ts
+++
@@ -1,9 +0,0 @@
-import express from "express";
-import { createJob, deleteJob, getAllJobs, getJobById, updateJob } from "../application/jobs";
-
-const jobsRouter = express.Router();
-
-jobsRouter.route("/").get(getAllJobs).post(createJob);
-jobsRouter.route("/:_id").get(getJobById).put(updateJob).delete(deleteJob);
-
-export default jobsRouter;
